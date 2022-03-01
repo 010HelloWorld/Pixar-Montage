f916b1d5b4f58c558554be4f1ea555d6588cd82b
010HelloWorld <99000535+010HelloWorld@users.noreply.github.com>
2022-02-28 22:15:51 -0700

    update

---
+++ Pixar Montage/Assets/Scripts/Notes.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    /* pixar lamp info https://sketchfab.com/3d-models/pixar-lamp-model-blender-5697e9cccdca4aa791000a3e2dffe784
+     * https://sketchfab.com/harteshkumar38
+     * 
+     * */
+}
